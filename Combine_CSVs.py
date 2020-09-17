abf93ecfc16948c899364e253ba9203e90d3fa4d
Anuj-Goria <61735372+Anuj-Goria@users.noreply.github.com>
2020-09-18 00:18:26 +0530

    Update Combine_CSVs.py
    Added functionality to drop columns

--- Combine_CSVs.py
+++ Combine_CSVs.py
@@ -19,8 +19,13 @@ os.chdir("/mydir")
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #print(all_filenames)
-
+                       
 #combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+
+#If you want to drop columns while combining csvs, simply use df.drop followed by column name
+combined_csv = df.drop(columns= (['Column A','Column B','Column C'])
+#Print top 5 rows
+print(df.head(5))
 #export to csv
 combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
